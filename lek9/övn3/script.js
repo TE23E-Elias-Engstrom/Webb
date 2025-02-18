dc65904e6f132301db5e00c86460bf31c5e10dee
Elias Engström <elias.engstrom@elev.ntig.se>
2025-02-18 13:46:28 +0100

    gjort lite js

--- lek9/övn3/script.js
+++ lek9/övn3/script.js
@@ -1,25 +1,38 @@
-let slumptal =parseInt(Math.random()*7);
+let slumptal = parseInt(Math.random() * 7);
 let länder = ["Holland", "Belgien", "Italien", "Rumäninen", "Albanien", "Tjenkien", "Polen", "Österrike"];
 let städer = ["Amsterdam", "Brüssel", "Rom", "Bukarest", "Tirana", "Prag", "Warszawa", "Wien"];
 
-let p_fråga=document.querySelector("#fråga");
-let input_svar=document.querySelector("#svar");
-let p_resultat=document.querySelector("#resultat");
+let p_fråga = document.querySelector("#fråga");
+let input_svar = document.querySelector("#svar");
+let p_resultat = document.querySelector("#resultat");
 
-function fråga(){
-    slumptal =parseInt(Math.random()*7);
+function fråga() {
+    slumptal = parseInt(Math.random() * länder.length);
     let fråga = länder[slumptal]
-    p_fråga.textContent="Ange huvudstad för "+fråga
+    p_fråga.textContent = "Ange huvudstad för " + fråga
 }
 
-let poäng=0;
-function ange_svar(){
-    let svar = input_svar.value ;
-    p_resultat.textContent="Rätt svar: "+ städer[slumptal];
-    
-    input_svar.value="";
+let poäng = 0;
+function ange_svar() {
+    let svar = input_svar.value;
+    p_resultat.textContent = "Rätt svar: " + städer[slumptal];
 
-    if(svar==städer[slumptal])
+    input_svar.value = "";
+
+    if (svar == städer[slumptal])
         poäng++;
-    p_resultat.textContent += ", poäng="+poäng;
+    p_resultat.textContent += ", poäng=" + poäng;
 }
+
+let input_ny_land = document.querySelector("#ny_land");
+let input_ny_stad = document.querySelector("#ny_stad");
+
+function ny_fråga() {
+    let ny_land = input_ny_land.value;
+    let ny_stad = input_ny_stad.value;
+    länder.push(ny_land);
+    städer.push(ny_stad);
+    input_ny_land.value = "";
+    input_ny_stad.value = "";
+}
+
